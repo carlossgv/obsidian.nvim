0d4e3ef64a94eb552e1d55ba374cb661fe91f080
Carlos Gonzalez <carlossgv@gmail.com>
2024-06-25 15:28:28 -0600

    feat: add images assets in current file folder

--- lua/obsidian/commands/paste_img.lua
+++ lua/obsidian/commands/paste_img.lua
@@ -1,10 +1,20 @@
 local Path = require "obsidian.path"
 local util = require "obsidian.util"
 local paste_img = require("obsidian.img_paste").paste_img
+local get_current_buffer_path = function()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local path = vim.api.nvim_buf_get_name(bufnr)
+  if path == "" then
+    return ""
+  end
+
+  path = path:match "(.*/)"
+  return path
+end
 
 ---@param client obsidian.Client
 return function(client, data)
-  local img_folder = Path.new(client.opts.attachments.img_folder)
+  local img_folder = Path.new(get_current_buffer_path() .. client.opts.attachments.img_folder)
   if not img_folder:is_absolute() then
     img_folder = client.dir / client.opts.attachments.img_folder
   end
